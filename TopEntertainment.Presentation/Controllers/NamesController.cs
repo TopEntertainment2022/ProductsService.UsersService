627da8356a4c73b38fdccac8ef6c7ada20130d87
claudiodamico <cdamico18@gmail.com>
2022-04-12 10:30:29 -0300

    first commit

---
+++ TopEntertainment.Presentation/Controllers/NamesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TopEntertainment.Domain.DTOs;
+
+namespace TopEntertainment.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class NamesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
