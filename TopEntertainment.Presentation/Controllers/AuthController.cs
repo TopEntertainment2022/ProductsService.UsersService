c79baf68be644e3526ed7a6bf9ac262d46cdb9ad
claudiodamico <cdamico18@gmail.com>
2022-04-27 10:57:21 -0300

    Creado el controller de autenticacion y endpoints de registro y login

---
+++ TopEntertainment.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TopEntertainment.Presentation.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Ok:False - " + ex.Message);
+            }
+        }
+    }
+}
